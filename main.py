fcf6589c1a56f6c41cbe90bfa204d9bb8e05fb12
Kyung Jae Kim <kyung_jae.kim@spaceforce.mil>
2026-09-04 11:53:35 -0400

    Set up variables to store user input

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 def main():
-    pass
+    # Needed variables:
+    member_last_name = ''
+    member_first_name = ''
+    member_middle_initial = ''
+    member_rank = ''
+    member_grade = ''
+    member_email = ''
+    member_phone = ''
+    shift_date = ''
+    shift_start_time = ''
+    task_assigned = ''
+    task_completed = ''
+    task_remaining = ''
+    task_remarks = ''
+    shift_end_time = ''
 
 if __name__ == "__main__":
     pass
